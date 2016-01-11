2773a74de47e5587d6682a30393d8eafe2b5a6e8
Wyatt Preul <wpreul@gmail.com>
2016-01-11 15:19:50 -0600

    lint

--- test/fire.test.js
+++ test/fire.test.js
@@ -14,35 +14,28 @@ var FAF = require('..')
 
 describe('fire-and-forget', function () {
   it('Fired a pattern which doesn\'t exist and forgot', function (done) {
-    var seneca = Seneca()
+    var seneca = Seneca({ log: 'silent' })
     seneca.use(FAF)
 
-    // We don't expect anything here, so timeout is the best way to handle
-    // expected behaviour
-
     seneca.ready(function () {
-      seneca.fire({ role: 'foo' }, function () {
-        done(Error('unexpectedly got into .fire callback (???)'))
+      seneca.fire({ role: 'foo' }, function (err) {
+        expect(err).to.exist()
+        done()
       })
     })
-
-    setTimeout(function () {
-      done()
-    }, 3000)
   })
 
   it('Fired a pattern which exist and got data back', function (done) {
-    var seneca = Seneca()
+    var seneca = Seneca({ log: 'silent' })
     seneca.use(FAF)
-
-    // We expect data back, so no need for a timeout
 
     seneca.add({ role: 'foo' }, function (args, done) {
       done(null, { msg: 'success' })
     })
 
     seneca.ready(function () {
-      seneca.fire({ role: 'foo' }, function (reply) {
+      seneca.fire({ role: 'foo' }, function (err, reply) {
+        expect(err).to.not.exist()
         expect(reply.msg).to.equal('success')
         done()
       })
@@ -50,10 +43,8 @@ describe('fire-and-forget', function () {
   })
 
   it('Fired a pattern which exists and but didn\'t care about a response', function (done) {
-    var seneca = Seneca()
+    var seneca = Seneca({ log: 'silent' })
     seneca.use(FAF)
-
-    // We don't expect any reply, so a timeout is the best measure of success
 
     seneca.add({ role: 'foo' }, function (args, done) {
       done(null, { msg: 'success' })
@@ -61,27 +52,17 @@ describe('fire-and-forget', function () {
 
     seneca.ready(function () {
       seneca.fire({ role: 'foo' })
-    })
-
-    setTimeout(function () {
       done()
-    }, 3000)
+    })
   })
 
   it('Fired a pattern which didn\'t exist and and didn\'t care about a response', function (done) {
-    var seneca = Seneca()
+    var seneca = Seneca({ log: 'silent' })
     seneca.use(FAF)
-
-    // It's completely okay to just fire a random pattern here
-    // We don't expect to get a reply anyway
 
     seneca.ready(function () {
       seneca.fire({ role: 'foo' })
-    })
-
-    setTimeout(function () {
-      // if it hasn't failed, this should run
       done()
-    }, 3000)
+    })
   })
 })
